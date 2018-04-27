3d147a048463b4188e339fc1d5c9fa39dd6031a8
divya <divya@bsetxt.com>
2018-04-27 13:24:10 +0530

    commit check

--- app/src/main/java/com/example/bsetec/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bsetec/myapplication/MainActivity.java
@@ -1,21 +1,9 @@
 package com.example.bsetec.myapplication;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -28,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bbbview = (WebView) findViewById(R.id.bbbview);
+
+
+        //added bb view
 
        // bbbview.getSettings().setJavaScriptEnabled(true);
        // bbbview.getSettings().setDomStorageEnabled(true);
